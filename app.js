2ec4323ced26d696e6e30b8b09307e46e12824ff
Arowolo Babajide <babajidearowolo5@gmail.com>
2026-03-01 20:08:59 +0000

    all

---
+++ app.js
@@ -0,0 +1,66 @@
+const startButton = document.getElementById("start-button");
+
+if (startButton) {
+    startButton.addEventListener("click", () => {
+        window.location.href = "sections.html";
+    })
+}
+
+
+let currentStep = 0;
+const cards = document.querySelectorAll('.onboarding_main');
+const dots = document.querySelectorAll('.dot');
+const nextButtons = document.querySelectorAll('.next-btn');
+const back = document.querySelectorAll(".back-link");
+
+
+function showCard(step) {
+    cards.forEach((card, index) => {
+        card.style.display = index === step ? 'flex' : 'none';
+    });
+    
+
+    dots.forEach((dot, index) => {
+        dot.classList.toggle('active', index === step);
+    });
+}
+
+
+nextButtons.forEach((button, index) => {
+    button.addEventListener('click', () => {
+        if (index === currentStep) {
+            currentStep++;
+            if (currentStep < cards.length) {
+                showCard(currentStep);
+            } else {
+                
+                window.location.href = "sections.html";
+            }
+        }
+    });
+});
+
+back.forEach((button, index) => {
+    button.addEventListener('click', () => {
+        if (index === currentStep) {
+            currentStep--;
+            if (currentStep > cards.length) {
+                showCard(currentStep);
+            }else {
+                window.location.href = "sections.html";
+            }
+        }
+    });
+});
+
+
+dots.forEach((dot, index) => {
+    dot.addEventListener('click', () => {
+        currentStep = index;
+        showCard(currentStep);
+    });
+});
+
+showCard(0);
+
+  
